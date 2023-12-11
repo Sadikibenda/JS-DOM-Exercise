67df538b597b9edc80478b608d4aad259a396028
Sadikibenda <Sadiki.benda1@gmail.com>
2023-12-11 16:44:16 -0500

    update

--- DOM.js
+++ DOM.js
@@ -1,24 +1,23 @@
          
          // Get the header element 
-let header = document.getElementsByTagName("header")
+let header = document.getElementsByTagName("header")[0]
 
     // Get the section elements 
 let sections = document.getElementsByTagName("section")
 
    // Get the section element with class = "current"
-let Current = document.getElementsByClassName('current')
+let Current = document.getElementsByClassName('current')[0]
 
 
   // Get the section that comes after the current section       ?????? use next sibling
-let sectioncomesAftercurrent = Current[0].nextElementSibling
-
-//let sectioncomesAftercurrent = document.getElementsByClassName('current').nextSibling
+let sectioncomesAftercurrent = Current.nextElementSibling
 
   //get the h2 node form the section before the 'current' section    ???????????? use previous sibling
-let sectionBeforethecurrent = Current[0].previousSibling
+let sectionBeforethecurrent = Current.previousElementSibling.firstElementChild
 
   // Get the div that contains the section that has an h2 with a class of 'highlight'
-let  div = document.getElementsByClassName('highlight')
+//let  div = document.querySelector('div section h2.highlight').parentElement.parentElement
+let div = document.querySelector('div')
 
   // Get all the section that contain an H2 (using a single statement)
 let sectionwithH2 = document.querySelectorAll('section h2')
